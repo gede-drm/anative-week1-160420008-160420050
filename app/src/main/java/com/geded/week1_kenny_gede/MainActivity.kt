4e718a7fe588738dcc74eebf912504c00a99c0dd
kenn0808 <kenkwando08@gmail.com>
2023-03-01 15:58:13 +0700

    Added Button Click Me

--- app/src/main/java/com/geded/week1_kenny_gede/MainActivity.kt
+++ app/src/main/java/com/geded/week1_kenny_gede/MainActivity.kt
@@ -2,10 +2,17 @@ package com.geded.week1_kenny_gede
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        btnToast.setOnClickListener{
+            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
